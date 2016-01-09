0342b002ae33303e022177f77dc15176dcdabc65
jeevb <me@jeev.io>
2016-01-08 22:41:22 -0800

    Add support for client-side certificates to verify requests.

--- prime/notify.py
+++ prime/notify.py
@@ -5,15 +5,16 @@ class PrimeNotifyAPI(PostAPIRequest):
     base_url = 'https://{host}:{port}'
     endpoint = '/notify/'
 
-    def __init__(self, host, port, token):
+    def __init__(self, host, port, token, cert):
         self._host = host
         self._port = port
         self._token = token
+        self._cert = cert
 
     def __call__(self, text, to=None):
         return self.response(
             url_args={'host': self._host, 'port': self._port},
             headers={'Authorization': 'Token {}'.format(self._token)},
             data={'text': text, 'to': to or []},
-            verify=False
+            verify=self._cert
         )
